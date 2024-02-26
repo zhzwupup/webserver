ea45c598bfdddf0d37346a4fa4b7c1f099dd4987
ChJh <legendtale@163.com>
2024-02-27 00:01:02 +0800

    Add: Channel handleEvent ut

--- tests/ChannelTest.cpp
+++ tests/ChannelTest.cpp
@@ -1,6 +1,6 @@
 #include "Channel.h"
-#include "doctest.h"
 #include "EventLoop.h"
+#include "doctest.h"
 #include <spdlog/sinks/stdout_sinks.h>
 #include <sys/epoll.h>
 
@@ -19,6 +19,24 @@ Channel::Channel(EventLoop *loop, int fd)
 
 void Channel::update() { logger->info("m_loop->updateChannel(this)"); }
 
+void Channel::handleEvent() {
+  if (m_revents & (EPOLLIN | EPOLLPRI)) {
+    if (m_readCallback) {
+      m_readCallback();
+    }
+  }
+  if (m_revents & EPOLLOUT) {
+    if (m_writeCallback) {
+      m_writeCallback();
+    }
+  }
+  if (m_revents & EPOLLERR) {
+    if (m_errorCallback) {
+      m_errorCallback();
+    }
+  }
+}
+
 TEST_CASE("Enable Reading") {
   ringbuffer_sink->set_pattern("%v");
   sinks.push_back(ringbuffer_sink);
@@ -31,4 +49,41 @@ TEST_CASE("Enable Reading") {
   std::string expected{"m_loop->updateChannel(this)\n"};
   std::string text = ringbuffer_sink->last_formatted().back();
   CHECK(expected == text);
+}
+
+TEST_CASE("Set Callback and handle event") {
+  ringbuffer_sink->set_pattern("%v");
+  sinks.push_back(ringbuffer_sink);
+  logger = std::make_shared<spdlog::logger>("logger_name", std::begin(sinks),
+                                            std::end(sinks));
+  EventLoop loop;
+  Channel channel(&loop, 0);
+  auto read_cb = []() { logger->info("readCallback"); };
+  auto write_cb = []() { logger->info("writeCallback"); };
+  auto error_cb = []() { logger->info("errorCallback"); };
+  channel.setReadCallback(read_cb);
+  channel.setWriteCallback(write_cb);
+  channel.setErrorCallback(error_cb);
+
+  SUBCASE("read callback") {
+    channel.set_revents(EPOLLIN);
+    channel.handleEvent();
+    std::string expected{"readCallback\n"};
+    std::string text = ringbuffer_sink->last_formatted().back();
+    CHECK(expected == text);
+  }
+  SUBCASE("write callback") {
+    channel.set_revents(EPOLLOUT);
+    channel.handleEvent();
+    std::string expected{"writeCallback\n"};
+    std::string text = ringbuffer_sink->last_formatted().back();
+    CHECK(expected == text);
+  }
+  SUBCASE("error callback") {
+    channel.set_revents(EPOLLERR);
+    channel.handleEvent();
+    std::string expected{"errorCallback\n"};
+    std::string text = ringbuffer_sink->last_formatted().back();
+    CHECK(expected == text);
+  }
 }
