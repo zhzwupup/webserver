10cded166a476775bb50493752333427e3e5bf54
ChJh <legendtale@163.com>
2024-03-17 03:13:54 +0800

    Modify: 生成 so

--- src/xmake.lua
+++ src/xmake.lua
@@ -1,7 +1,13 @@
 add_rules("mode.debug", "mode.release")
 
+toolchain("gcc13")
+    set_kind("standalone")
+    set_bindir("/opt/gcc13-2/bin/")
+
 target("simple")
   set_kind("shared")
   add_includedirs("../include/")
   add_links("spdlog", "fmt")
-  add_files("EventLoop.cpp")
+  add_files("*.cpp")
+  set_toolchains("gcc13")
+  add_cxxflags("-std=c++23")
